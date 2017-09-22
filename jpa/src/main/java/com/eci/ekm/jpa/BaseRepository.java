1ddbfa61d8f1cac72cf023c1e283b6a221728210
arahim <arahim@ARAHIM02.ecitele.com>
2017-09-22 19:24:58 +0530

    Update Compile Errors

--- jpa/src/main/java/com/eci/ekm/jpa/BaseRepository.java
+++ jpa/src/main/java/com/eci/ekm/jpa/BaseRepository.java
@@ -1,9 +1,5 @@
 package com.eci.ekm.jpa;
 
 public class BaseRepository {
-<<<<<<< HEAD
-// SYstem out println update again
-=======
-// update from web
-// >>>>>>> branch 'JPA-Support' of https://github.com/arah786/EKM
+// System.out.printLn("TEst");
 }
